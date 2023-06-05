aa3cd1b1c460ba16c402f3e75df14f16ff174617
lindashao1220 <ys4950@nyu.edu>
2023-06-05 23:03:49 +0800

    chjanges

--- myFirst.cpp
+++ myFirst.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-void main()
-{
-    using namespace std;
-    cout <<"hello.";
-    cout endl;
+#include <string>
+
+int main() {
+    std::string name;
+    int age;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    std::cout << "Hello, " << name << "!" << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
+
     return 0;
 }
