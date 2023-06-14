c340345902c3cbf08cb6fbe4aeb5abd05761706b
lindashao1220 <ys4950@nyu.edu>
2023-06-14 16:30:22 +0800

    xss

--- nodeAgain/index.js
+++ nodeAgain/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.listen(3000, ()=>console.log("listening at port 3000"));
+app.listen(3000, ()=>console.log("I am listening on 3000lalala"));
 app.use(express.static('public'));
