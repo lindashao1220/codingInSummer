504f7fac0f1ff669c2201d577de741e118f833af
lindashao1220 <ys4950@nyu.edu>
2023-06-15 18:14:41 +0800

    xhages

--- nodeAgain/index.js
+++ nodeAgain/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(3000, ()=>console.log("I am listening on 3000lalala"));
+app.listen(3000, () => console.log("I am listening on 3000lalala"));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb'}));
 
-console.log("jss");
+const database = [];
 
 app.post('/api', (request, response)=>{
     console.log("yaya")
     console.log(request.body);
-    console.log("i am here");
+    const data = request.body;
+    database.push(data);
+    console.log(database);
     //发response给index。html
     response.json({
         status: 'success',
-        lat: request.body.lat
+        lat: data.lat
     })
 })
