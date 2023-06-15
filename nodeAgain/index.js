61d7b752488a9d43165d61ff2c4de2ed68ce5244
lindashao1220 <ys4950@nyu.edu>
2023-06-15 17:51:56 +0800

    che

--- nodeAgain/index.js
+++ nodeAgain/index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const app = express();
 app.listen(3000, ()=>console.log("I am listening on 3000lalala"));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+console.log("jss");
+
+app.post('/api', (request, response)=>{
+    console.log("yaya")
+    console.log(request.body);
+    console.log("i am here");
+    //发response给index。html
+    response.json({
+        status: 'success',
+        lat: request.body.lat
+    })
+})
