fdc90f5e2c27d0ac9580247b68384c5f003b0fa4
lindashao1220 <ys4950@nyu.edu>
2023-07-03 17:31:42 +0800

    chanes

---
+++ for...of/script.js
@@ -0,0 +1,13 @@
+//bubbles 是一个 array
+//以下两种是一个意思
+
+for(let ele of bubbles){
+    ele.move();
+    ele.display()
+}
+
+
+for(let i = 0; i < bubbles; i++){
+    ele[i].move();
+    ele[i].display()
+}
